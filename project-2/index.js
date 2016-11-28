be7a7620fb1013d466abac6594ae12f0c14803cb
jp <gaines.johnp@gmail.com>
2016-11-27 22:03:00 -0800

    added challenge questions

--- project-2/index.js
+++ project-2/index.js
@@ -1,19 +1,5 @@
-//by adding .children to $(this) we get back the arrow functionality as demonstrated by the console.log. once the .toggleClass is re-attached functionality is the same as before.
-
 $(".question").click(function(){
   $(this).next().slideToggle("slow");
   // console.log($(this).children());
   $(this).children().toggleClass("collapse");
-  // $("#a1").slideToggle("slow");
-  // $("#arrow1-down, #arrow1-up").toggleClass("collapse");
 })
-
-// $("#q2").click(function(){
-//   $("#a2").slideToggle("slow");
-//   $("#arrow2-down, #arrow2-up").toggleClass("collapse");
-// })
-
-// $("#q3").click(function(){
-//   $("#a3").slideToggle("slow");
-//   $("#arrow3-down, #arrow3-up").toggleClass("collapse");
-// })
